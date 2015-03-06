68d1ee093229814986bbd9bcdc2823d845772175
DFAhmed <f.ahmed@tue.nl>
2015-03-06 17:20:48 +0100

    deleted files

--- plot1.R
+++
@@ -1,4 +0,0 @@
-Data<-read.csv('D:/Coursera/ExploratoryDataAnalysis/exdata_data_household_power_consumption/household_power_consumption.txt',sep=';',na.strings="?");
-data2<-Data[Data$Date=="1/2/2007" | Data$Date=="2/2/2007",];
-
-hist(data2$Global_active_power,col='red',xlab='Global Active Power (Killowatts)',main = 'Global Active Power');
