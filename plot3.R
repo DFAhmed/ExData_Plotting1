9adce47a7c1971c12b6a7daf800783032fd65f66
DFAhmed <f.ahmed@tue.nl>
2015-03-06 16:21:40 +0100

    Plots Included
    Plots with R files are included.

---
+++ plot3.R
@@ -0,0 +1,9 @@
+Data<-read.csv('D:/Coursera/ExploratoryDataAnalysis/exdata_data_household_power_consumption/household_power_consumption.txt',sep=';',na.strings="?");
+data2<-Data[Data$Date=="1/2/2007" | Data$Date=="2/2/2007",];
+
+plot(data2$Sub_metering_1,type='l',ylab='Energy sub metering',xlab='',xaxt='n',col="black")
+lines(data2$Sub_metering_2,type='l',ylab='Energy sub metering',xlab='',xaxt='n',col="red")
+points(data2$Sub_metering_3,type='l',ylab='Energy sub metering',xlab='',xaxt='n',col="blue")
+legend(length(data2$Sub_metering_1)-1000,37, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.8, 
+       col=c("black","red","blue"),  lty=1);
+axis(1,labels=c('Thu','Fri','Sat'),at=c(0,1500,length(data2$Global_active_power)))
